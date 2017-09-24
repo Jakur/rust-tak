e9f6df8a716e25a6408aa47b8ccc0ec94b198865
Justin Kur <JustinK97@hotmail.com>
2017-09-23 22:44:35 -0400

    Initial dump of many components for the game
    This includes the board and the lesser structs within, checking of the
    game-ending conditions, as well as movement logic and ptn parsing (though
    these are less thoroughly tested). The rules of the game are kept in a
    trait called RuleSet in order to keep the rules generic enough to allow
    variants to be constructed with relative ease. Most of the essentials for
    Tak are present now, but they are disjointed and work will have to be
    done to integrate them.

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![feature(test)]
+
+extern crate ndarray;
+extern crate test;
+#[macro_use] extern crate lazy_static;
+extern crate regex;
+
+mod game;
+use game::*;
+
+fn main() {
+    game::example();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn search_bench(b: &mut Bencher) {
+        use game::Color;
+        use game::Player;
+        use game::RuleSet;
+        let p1 = Player {
+            color: Color::White,
+            pieces: 0,
+            caps: 0,
+        };
+        let p2 = Player {
+            color: Color::Black,
+            pieces: 0,
+            caps: 0,
+        };
+        let r = StandardRules::new(State::new(5, p1, p2));
+        let mut game = Game::new(r);
+        for x in 1..game.rules.state.size {
+            game.rules.place_w_flat((0, x));
+            game.rules.place_w_flat((1, x));
+            game.rules.place_w_flat((2, x));
+            game.rules.place_w_flat((3, x));
+            game.rules.place_w_flat((4, x));
+        }
+        println!("\n{:?}", &game.rules.state.board);
+        b.iter(|| game.rules.check_win());
+    }
+}
+
